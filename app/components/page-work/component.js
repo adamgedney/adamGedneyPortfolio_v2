79fe83729488322761e00bc2cdfcb562a0dc7164
Adam Gedney <adam.gedney@gmail.com>
2016-04-20 21:45:32 -0400

    WORKING on adding portfolio items

--- app/components/page-work/component.js
+++ app/components/page-work/component.js
@@ -9,163 +9,173 @@ const imagePath = '/images/codeTools/';
 const projects = [
 	{
 		id : 1,
-		title:'Trello Clone',
-		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-		featuredImage:`http://placekitten.com/1200/900`,
+		title:'GenSizer SPA',
+		description:'A generator size estimation tools for Assurance Power',
+		featuredImage:`/images/work/alliedGen/landing.jpeg`,
 		screenshots:[
-			{path:`http://placekitten.com/500/400`,
-				title:'Screenshot #1',
-				description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipiscing elit,',
-				features : [
-					{description: 'Javascript, HTML5, CSS3, Sass', subDescription:'ES6'},
-					{description: 'Javascript', subDescription:'ES6'},
-					{description: 'Javascript', subDescription:'ES6'},
-					{description: 'Javascript', subDescription:'ES6'},
-				]
+			{path:`/images/work/alliedGen/landing.jpeg`,
+				title:'Site landing',
+				description:''
 			},
-			{path:`http://placekitten.com/500/400`,
-				title:'Screenshot #1',
-				description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
-				features : [
-					{description: 'Javascript', subDescription:'ES6'},
-					{description: 'Javascript', subDescription:'ES6'},
-					{description: 'Javascript', subDescription:'ES6'},
-					{description: 'Javascript', subDescription:'ES6'}
-				]
+			{path:`/images/work/alliedGen/percent.jpeg`,
+				title:'Percentage interface',
+				description:''
 			},
-			{path:`http://placekitten.com/500/400`,
-				title:'Screenshot #1',
-				description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
-				features : [
-					{description: 'Javascript', subDescription:'ES6'},
-					{description: 'Javascript', subDescription:'ES6'},
-					{description: 'Javascript', subDescription:'ES6'},
-					{description: 'Javascript', subDescription:'ES6'},
-					{description: 'Javascript', subDescription:'ES6'},
-					{description: 'Javascript', subDescription:'ES6'},
-				]
-			}
+			{path:`/images/work/alliedGen/tons.jpeg`,
+				title:'CSS Scrollwheel selector',
+				description:''
+			},
+			{path:`/images/work/alliedGen/type.jpeg`,
+				title:'Type selector',
+				description:''
+			},
 		]
 	},
 	{
 		id : 2,
-		title:'Trello Clone',
-		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-		featuredImage:`http://placekitten.com/1200/900`,
+		title:'Company Portal',
+		description:'A company portal providing internal tools and interfaces for the company AWS microservice API.',
+		featuredImage:`/images/work/companyPortal/portal_dashboard.png`,
 		screenshots:[
-			{path:`http://placekitten.com/200/300`,
-				title:'Screenshot #1',
-				description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
+			{path:`/images/work/companyPortal/portal_dashboard.png`,
+				title:'Portal Dashboard',
+				description:''
+			},
+			{path:`/images/work/companyPortal/1_catalog_module_list_view.png`,
+				title:'Catalog Manager List View',
+				description:''
+			},
+			{path:`/images/work/companyPortal/2_catalog_module_user_role_management_modal.png`,
+				title:'User Role Management',
+				description:''
+			},
+			{path:`/images/work/companyPortal/3_catalog_module_create_modal.png`,
+				title:'Create Modal',
+				description:''
+			},
+			{path:`/images/work/companyPortal/5_catalog_module_create_series_modal.png`,
+				title:'Series Creation',
+				description:''
+			},
+			{path:`/images/work/companyPortal/6_catalog_module_series_page_top.png`,
+				title:'Series View Top',
+				description:''
+			},
+			{path:`/images/work/companyPortal/7_catalog_module_series_page_bottom.png`,
+				title:'Series View Bottom',
+				description:''
+			},
+			{path:`/images/work/companyPortal/8_catalog_module_title_page_top.png`,
+				title:'movie View',
+				description:''
+			},
+			{path:`/images/work/companyPortal/9_catalog_module_title_page_bottom.png`,
+				title:'Versions View',
+				description:''
 			}
 		]
 	},
 	{
 		id : 3,
-		title:'Trello Clone',
-		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-		featuredImage:`http://placekitten.com/1200/900`,
+		title:'Graphic Design, Digital Art, & Logos',
+		description:'A collection of some of the graphic & logo design work I\'ve done or worked on',
+		featuredImage:`/images/work/graphic/pbzAd.png`,
 		screenshots:[
-			{path:`http://placekitten.com/200/300`,
-				title:'Screenshot #1',
-				description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
+			{path:`/images/work/graphic/pbzAd.png`,
+				title:'A Florida Magazine Advertisement',
+				description:''
+			},
+			{path:`/images/work/graphic/BizCard-Meditrina-BizCardMockup2300x1673.jpg`,
+				title:'Napa Valley Concierge Service Logo Design and Business Cards',
+				description:''
+			},
+			{path:`/images/work/graphic/bottle-09ck.png`,
+				title:'Wine Label Edits and ATF Approval Adjustments',
+				description:''
+			},
+			{path:`/images/work/graphic/Consider.png`,
+				title:'Digital Art',
+				description:''
+			},
+			{path:`/images/work/graphic/considerSharpening.png`,
+				title:'Digital Art',
+				description:''
+			},
+			{path:`/images/work/graphic/Cop-ernicus.png`,
+				title:'Digital Art',
+				description:''
+			},
+			{path:`/images/work/graphic/gedney_adam_p1.jpg`,
+				title:'Adobe Illustrator Gradient Mesh Art',
+				description:''
+			},
+			{path:`/images/work/graphic/Gedney_Adam_Travel_Poster.png`,
+				title:'Travel Poster',
+				description:''
+			},
+			{path:`/images/work/graphic/Gedney_Adam_Travel_Poster3.png`,
+				title:'Travel Poster',
+				description:''
+			},
+			{path:`/images/work/graphic/GraphicDesign-OrbitsAd-Mockup800x600.jpg`,
+				title:'Adobe Illustrator Mock Advertisement for Orbitz',
+				description:''
+			},
+			{path:`/images/work/graphic/Logo-COW-Mockup800x600.jpg`,
+				title:'Napa Valley Winery Logo',
+				description:''
+			},
+			{path:`/images/work/graphic/Logo-StudioByKeliann-Mockup.png`,
+				title:'Hair Salon Logo',
+				description:''
+			},
+			{path:`/images/work/graphic/mono.png`,
+				title:'Digital Art',
+				description:''
+			},
+			{path:`/images/work/graphic/Print-AubertOrder-Mockup1500x956.jpg`,
+				title:'Napa Valley Exclusive Winery Order Form',
+				description:''
+			},
+			{path:`/images/work/graphic/Print-MayaMenu-Mockup1500x1017.jpg`,
+				title:'Napa Valley Bar Menu',
+				description:''
+			},
+			{path:`/images/work/graphic/Print-TerroirCalistogaPostCard-Mockup.png`,
+				title:'Wine Club Offer Postcard Advertisement',
+				description:''
+			},
+			{path:`/images/work/graphic/Print-ZosimaPostCard-Mockup1920x1280.jpg`,
+				title:'Post Card Mailer',
+				description:''
 			}
 		]
 	},
 	{
 		id : 4,
-		title:'Trello Clone',
-		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-		featuredImage:`http://placekitten.com/1200/900`,
+		title:'Jericho Canyon Vineyard',
+		description:'A WordPress Website & Graphic Design',
+		featuredImage:`/images/work/jericho/label-ck09Bottle.png`,
 		screenshots:[
-			{path:`http://placekitten.com/200/300`,
-				title:'Screenshot #1',
-				description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
-			}
+			{path:`/images/work/jericho/label-ck09Bottle.png`,
+				title:'Wine Label Work',
+				description:''
+			},
+			{path:`/images/work/jericho/JerichoCanyonVineyardWebsite.png`,
+				title:'100+ Page Custom WordPress Site',
+				description:''
+			},
+			{path:`/images/work/jericho/label-ch10toppic.png`,
+				title:'Wine Label Work',
+				description:''
+			},
+			{path:`/images/work/jericho/label-ootoppic.png`,
+				title:'Wine Label Work',
+				description:''
+			},
 		]
 	},
-	{
-		id : 1,
-		title:'Trello Clone',
-		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-		featuredImage:`http://placekitten.com/1200/900`,
-		screenshots:[
-			{path:`http://placekitten.com/200/300`,
-				title:'Screenshot #1',
-				description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
-			}
-		]
-	},
-	{
-		id : 1,
-		title:'Trello Clone',
-		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-		featuredImage:`http://placekitten.com/1200/900`,
-		screenshots:[
-			{path:`http://placekitten.com/200/300`,
-				title:'Screenshot #1',
-				description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
-			}
-		]
-	},
-	{
-		id : 1,
-		title:'Trello Clone',
-		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-		featuredImage:`http://placekitten.com/1200/900`,
-		screenshots:[
-			{path:`http://placekitten.com/200/300`,
-				title:'Screenshot #1',
-				description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
-			}
-		]
-	},
-	{
-		id : 1,
-		title:'Trello Clone',
-		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-		featuredImage:`http://placekitten.com/1200/900`,
-		screenshots:[
-			{path:`http://placekitten.com/200/300`,
-				title:'Screenshot #1',
-				description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
-			}
-		]
-	},
-	{
-		id : 1,
-		title:'Trello Clone',
-		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-		featuredImage:`http://placekitten.com/1200/900`,
-		screenshots:[
-			{path:`http://placekitten.com/200/300`,
-				title:'Screenshot #1',
-				description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
-			}
-		]
-	},{
-		id : 1,
-		title:'Trello Clone',
-		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-		featuredImage:`http://placekitten.com/1200/900`,
-		screenshots:[
-			{path:`http://placekitten.com/200/300`,
-				title:'Screenshot #1',
-				description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
-			}
-		]
-	},
-	{
-		id : 1,
-		title:'Trello Clone',
-		description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-		featuredImage:`http://placekitten.com/1200/900`,
-		screenshots:[
-			{path:`http://placekitten.com/200/300`,
-				title:'Screenshot #1',
-				description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore'
-			}
-		]
-	}
+
 ];
 
 let $ = Ember.$,
